d3a1d399c41de0fdf337ac5af0c4668865436886
Kry765 <krzysztofkleka91@gmail.com>
2024-06-04 22:38:23 +0200

    ...

--- src/components/Navigation/NavigationItem/NavigationItem.jsx
+++ src/components/Navigation/NavigationItem/NavigationItem.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import styled from "./NavigationItem.module.scss";
 
-export const NavigationItems = ({ MenuElement }) =>
-  MenuElement.map((MenuElements, index) => (
-    <div key={index} className={styled.topMenu}>
-      <p key="index">{MenuElements}</p>
-    </div>
+export const NavigationItemDesktop = ({ MenuDesciption }) =>
+  MenuDesciption.map((MenuElements, index) => (
+    <>
+      <div className={styled.topMenuDesktop}>
+        <a key={index} href={MenuElements.link} className={styled.topMenuLink}>
+          {MenuElements.description}
+        </a>
+      </div>
+      <div className={styled.topMenuMobileItems}>
+        <a key={index} href={MenuElements.link}>
+          {MenuElements.description}
+        </a>
+      </div>
+    </>
   ));
 
-export default NavigationItems;
+export default NavigationItemDesktop;
--- src/components/Navigation/NavigationWrapper/NavigationWrapper.jsx
+++ src/components/Navigation/NavigationWrapper/NavigationWrapper.jsx
@@ -2,17 +2,50 @@ import React from "react";
 import styled from "../NavigationWrapper/NavigationWrapper.module.scss";
 import NavigationItem from "../NavigationItem/NavigationItem";
 import PhotographName from "../PhotographName/PhotographName";
-import { MdMenu } from "react-icons/md";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { FaFacebookSquare } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
 
 const NamePhoto = "Fotograf";
-const MenuElement = ["O mnie", "Kontakt", "Portfolio", "Instagram", "Facebook"];
+const MenuDesciption = [
+  {
+    link: "/home",
+    description: "Strona Główna",
+  },
+  {
+    link: "/contact",
+    description: "Kontakt",
+  },
+  {
+    link: "/aboutme",
+    description: "O nas",
+  },
+  {
+    link: "/instagram",
+    description: (
+      <>
+        <span className={styled.socialMenuText}>Instagram</span>
+        <RiInstagramFill className={styled.socialMenuIcon} />
+      </>
+    ),
+  },
+  {
+    link: "/facebook",
+    description: (
+      <>
+        <span className={styled.socialMenuText}>Facebook</span>
+        <FaFacebookSquare className={styled.socialMenuIcon} />
+      </>
+    ),
+  },
+];
 
 export const NavigationWrapper = () => (
   <>
-    <PhotographName NamePhoto={NamePhoto} />
     <div className={styled.navWrapper}>
-      <NavigationItem MenuElement={MenuElement} />
-      <MdMenu className={styled.BurgerMenu} />
+      <PhotographName NamePhoto={NamePhoto} />
+      <NavigationItem MenuDesciption={MenuDesciption} />
+      <HiMenuAlt3 className={styled.BurgerMenu} />
     </div>
   </>
 );
